54d4df232838a6e1f54539bd38e686c5fd30f8e4
Jules Ferreira <julesferreira@gmail.com>
2013-03-26 14:07:42 -0600

    teleport fix (jules)

--- Assets/Scripts/DoorController.js
+++ Assets/Scripts/DoorController.js
@@ -41,14 +41,35 @@ function OnTriggerEnter(collidee : Collider) {
 	
 	if (inboundObjects.Contains(obj)) {
 		// Whatever just entered came from another door, so don't send it back
-		return;
+		//return;
+	}
+	
+	// get destination position
+	targetDoor.incoming(obj.gameObject);
+	var destination_pos : Vector3 = targetDoor.transform.position;
+	var starting_pos: Vector3 = this.transform.position;
+	var door_width = 4; // estimation
+	var door_height = 3; // estimation
+	
+	// determine which direction we're teleporting
+	var x_offset : double = 0;
+	var y_offset : double = 0;
+	if (starting_pos.x > destination_pos.x) { // moving west
+		x_offset = -door_width;
+	}
+	else if (starting_pos.x < destination_pos.x) { // moving east
+		x_offset = door_width;
+	}
+	else if (starting_pos.y > destination_pos.y) { // moving north
+		y_offset = -door_height;
+	}
+	else if (starting_pos.y < destination_pos.y) { // moving south
+		y_offset = door_height;
 	}
 	
 	// Otherwise, teleport to the targetdoor
-	targetDoor.incoming(obj.gameObject);
-	var doorpos : Vector3 = targetDoor.transform.position;
-	obj.transform.position.x = doorpos.x;
-	obj.transform.position.y = doorpos.y;
+	obj.transform.position.x = destination_pos.x + x_offset;
+	obj.transform.position.y = destination_pos.y + y_offset;
 	
 	// If it's the sidekick, have the camera follow
 	if (obj.name == "Sidekick") {
